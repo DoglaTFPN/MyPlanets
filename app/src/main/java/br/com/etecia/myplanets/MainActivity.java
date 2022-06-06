9f5372c1b123d0c08fbf9367d7da5f119318dd71
Laboratorio-Info <douglas_saragoza@hotmail.com>
2022-06-06 19:41:19 -0300

    commit aula 06.06 p2

--- app/src/main/java/br/com/etecia/myplanets/MainActivity.java
+++ app/src/main/java/br/com/etecia/myplanets/MainActivity.java
@@ -3,12 +3,54 @@ package br.com.etecia.myplanets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listaPlanetas;
+
+    String nomePlaneta [] = {"Júpiter", "Lua", "Marte", "Mercúrio", "Neturno", "Plutão", "Saturno", "Sol", "Terra", "Urano", "Vênus"};
+
+    int imagemPlanetas[] = {R.drawable.jupiter, R.drawable.lua,
+            R.drawable.marte, R.drawable.mercurio, R.drawable.netuno,
+            R.drawable.plutao, R.drawable.saturno, R.drawable.sol,
+            R.drawable.terra, R.drawable.urano, R.drawable.venus};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaPlanetas = findViewById(R.id.idListaPlanetas);
+
+        MyAdapter adapter = new MyAdapter();
+
+        listaPlanetas.setAdapter(adapter);
+    }
+
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
